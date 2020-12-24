2855d9c2a0691ed6dac6b45f293a9ae8538b8639
bigdataisland <76485350+bigdataisland@users.noreply.github.com>
2020-12-25 07:15:46 +0800

    Add files via upload

--- LLTAPI.py
+++ LLTAPI.py
@@ -1,16 +1,13 @@
-import streamlit as st
-import pandas as pd
-import ipinfo
-from streamlit.report_thread import get_report_ctx
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
-from streamlit.server.server import Server
-
-from datetime import datetime, timedelta
-from pytz import timezone
 import pytz
-
+import ipinfo
+import pandas as pd
+import streamlit as st
 from tzwhere import tzwhere
+from datetime import datetime
+from geopy.geocoders import Nominatim
+from streamlit.server.server import Server
+from geopy.extra.rate_limiter import RateLimiter
+from streamlit.report_thread import get_report_ctx
 
 class SessionState(object):
     def __init__(self, **kwargs):
@@ -85,83 +82,25 @@ def main():
     st.title("Pre-interview Code Submission ")
 
     st.markdown("""
-    This app performs simple test For Accuenergy.com
-    * **Libraries:** base64, pandas, streamlit, Javascript, IPInfo
+    This app performs simple test for accuenergy.com
+    * **Libraries:** base64, pandas, streamlit, Javascript, IPInfo, Markdown 
     * **Source Code:** [Lionel Luo @github.com](https://github.com/bigdataisland/webdemo/blob/main/LLTAPI.py).
     """)
 
-
-    st.sidebar.title("Search Address")
-
-    # Widget to select your page, you can choose between radio buttons or a selectbox
-    #page = st.sidebar.selectbox("Please Select Action", tuple(pages.keys()))
-    #page = st.sidebar.radio("Select your page", tuple(pages.keys()))
-
-    #ip = st.request.remote_ip
-    # url = 'http://ipinfo.io/json'
-    # response = request.urlopen(url)
-    # data = json.load(response)
-    #
-    # st.write(data)
-
-    # ctx = get_report_ctx()
-    # id = ctx.session_id
-    # st.write(ctx.session_id)
-    # for key, val in ctx.items():
-    #     st.write(key)
-    #     st.write(key)
-
-
-    #st.write(Server)
-
-    # if len(ssession.ws.request.headers['X-Forwarded-For'].split(',')[0]) > 0:
-    #     ip = '69.158.134.166'
-    # else:
-
-
-
-    #st.write("URL PARAM:" + str(urlpara))
-    #get_headers()
-
-    # st.write('Test')
-    # st.write(st.sidebar.text_input())
-    # st.write(page)
-    # # Display the selected page with the session state
-    # pages[page]()
-
-    #st.write(st.sidebar.text("Street").value)
-
+    st.sidebar.title("Search By Address")
     sessions = Server.get_current()._session_info_by_id
     session_id_key = list(sessions.keys())[0]
     session = sessions[session_id_key]
-    # st.write(session.ws.request.remote_ip)
-
-    # st.write(session.ws.request.headers)
-    # st.write(session.ws.request.headers['X-Forwarded-For'].split(',')[0])
-    # st.write(session.ws.request.uri)
-    # st.write(session.ws.request.host_name)
-    # st.write(session.ws.request.host)
-    # st.write(session.ws.request.host)
 
     if session.ws.request.remote_ip == '127.0.0.1':
         ip = session.ws.request.headers['X-Forwarded-For'].split(',')[0]
     else:
         ip = '123.59.195.125'
 
-    # st.write(ip)
-
-    # access_token = 'cbacbcd0adb278'
-    # handler = ipinfo.getHandler(access_token)
-    # ip_address = '123.59.195.125'
-    # details = handler.getDetails(ip_address)
-    # st.write(details.all)
-
     access_token = 'cbacbcd0adb278'
     handler = ipinfo.getHandler(access_token)
     ip_address = ip
     details = handler.getDetails(ip_address)
-
-    # st.write(details.all)
 
     my_street = ""
     my_city = details.city
@@ -173,29 +112,16 @@ def main():
     province = st.sidebar.text_input("Province", my_province)
     country = st.sidebar.text_input("Country", my_country)
 
-    if st.sidebar.button('Updatae'):
+    if st.sidebar.button('New Search'):
         pass
     else:
         pass
-
-
-    # st.sidebar.button('Updatae')
-
-    # lat = float(details.latitude)
-    # lon = float(details.longitude)
-    #
-    # st.write('lat: ' + str(lat))
-    # st.write('lon: ' + str(lon))
-    #
-    # map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
-    #
-    # # st.map(map_data)
-    # st.map(map_data, zoom=12)
 
     geolocator = Nominatim(user_agent="GTA Lookup")
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
     location = geolocator.geocode(street + ", " + city + ", " + province + ", " + country)
 
+    st.markdown(f"**Address Info Details**")
     st.write(location)
 
     lat = location.latitude
@@ -205,72 +131,38 @@ def main():
     tz = tzwhere.tzwhere()
     tz_name = tz.tzNameAt(lat, lon)
 
-    # st.write('Timezone: ' + tz_name)
-    # st.write('Latitude: ' + str(lat))
-    # st.write('Longitude: ' + str(lon))
-
-    st.write('Display Time')
+    st.markdown(f"**Time Information**")
     utc = pytz.utc
-    # utc.zone
+
     # 'UTC'
     utc_today = datetime.now(utc)
 
-
     timestamp = utc_today.timestamp()
-    #st.write('UTC Time : ' + str(utc_today))
-    #st.write('UTC Timestamp : ' + str(timestamp))
 
     tz = pytz.timezone(tz_name)
     local_today = datetime.now(tz)
-    #st.write('Local Time: ' + str(local_today))
 
-    st.markdown(f"Timezone: **{tz_name}**.")
-    st.markdown(f"UTC Timestamp: **{timestamp}**.")
-    st.markdown(f"Local Time: **{local_today}**.")
-
+    st.markdown(f"Timezone: ***{tz_name}***")
+    st.markdown(f"UTC Timestamp: ***{timestamp}***")
+    st.markdown(f"Local Time: ***{local_today}***")
 
     map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
-    # st.map(map_data)
 
-    st.write('Display the location in map')
+    #st.write('Display the location in map')
+    st.markdown(f"**Address Map**")
     st.map(map_data, zoom=12)
-
-
-
-
-
 
 def page_first():
     st.title("This is my first page")
     # ...
 
     session_state = get(remote_ip='', favorite_color='black')
-
-    #st.write(session_state.remote_ip)
-
     sessions = Server.get_current()._session_info_by_id
     session_id_key = list(sessions.keys())[0]
     session = sessions[session_id_key]
-    #st.write(session.ws.request.remote_ip)
 
-    #st.write(session.ws.request.headers)
-    #st.write(session.ws.request.headers['X-Forwarded-For'].split(',')[0])
-    #st.write(session.ws.request.uri)
-    #st.write(session.ws.request.host_name)
-    #st.write(session.ws.request.host)
-    #st.write(session.ws.request.host)
-
-    #ip = session.ws.request.headers['X-Forwarded-For'].split(',')[0]
+    # Default IP
     ip = '123.59.195.125'
-
-    #st.write(ip)
-
-    # access_token = 'cbacbcd0adb278'
-    # handler = ipinfo.getHandler(access_token)
-    # ip_address = '123.59.195.125'
-    # details = handler.getDetails(ip_address)
-    # st.write(details.all)
-
     access_token = 'cbacbcd0adb278'
     handler = ipinfo.getHandler(access_token)
     ip_address = ip
@@ -281,8 +173,6 @@ def page_first():
     city = st.sidebar.text_input("City", details.city)
     province = st.sidebar.text_input("Province", details.region)
     country = st.sidebar.text_input("Country", details.country_name)
-
-    #st.sidebar.button('Updatae')
 
     lat = float(details.latitude)
     lon = float(details.longitude)
@@ -296,22 +186,9 @@ def page_first():
     st.map(map_data, zoom=12)
 
 
-
-
-
 def page_second():
     st.title("This is my second page")
     # ...
-
-    # street = st.sidebar.text_input("Street", "75 Bay Street")
-    # city = st.sidebar.text_input("City", "Toronto")
-    # province = st.sidebar.text_input("Province", "Ontario")
-    # country = st.sidebar.text_input("Country", "Canada")
-
-    # street = st.sidebar.text("Street")
-    # city = st.sidebar.text("City")
-    # province = st.sidebar.text("Province")
-    # country = st.sidebar.text("Country")
 
     street = st.sidebar.text_input("Street", "")
     city = st.sidebar.text_input("City", "")
@@ -335,164 +212,6 @@ def page_second():
     # st.map(map_data)
     st.map(map_data, zoom=12)
 
-
-
-
 if __name__ == "__main__":
     main()
-
-# Streamlit - Settings page with session state
-# https://stackoverflow.com/questions/24678308/how-to-find-location-with-ip-address-in-python
-# https://gist.github.com/okld/0aba4869ba6fdc8d49132e6974e2e662
-
-# https://gist.github.com/tvst/036da038ab3e999a64497f42de966a92
-# https://github.com/streamlit/streamlit/blob/844691d0b15e0f3622a1fb97b34e4eae4161fe3f/lib/streamlit/ReportSession.py#L54
-
-# import streamlit as st
-# from streamlit.hashing import _CodeHasher
-#
-# try:
-#     # Before Streamlit 0.65
-#     from streamlit.ReportThread import get_report_ctx
-#     from streamlit.server.Server import Server
-# except ModuleNotFoundError:
-#     # After Streamlit 0.65
-#     from streamlit.report_thread import get_report_ctx
-#     from streamlit.server.server import Server
-#
-#
-# def main():
-#     state = _get_state()
-#     pages = {
-#         "Dashboard": page_dashboard,
-#         "Settings": page_settings,
-#     }
-#
-#     st.sidebar.title(":floppy_disk: Page states")
-#     page = st.sidebar.radio("Select your page", tuple(pages.keys()))
-#
-#     # Display the selected page with the session state
-#     pages[page](state)
-#
-#     # Mandatory to avoid rollbacks with widgets, must be called at the end of your app
-#     state.sync()
-#
-#
-# def page_dashboard(state):
-#     st.title(":chart_with_upwards_trend: Dashboard page")
-#     display_state_values(state)
-#
-#
-# def page_settings(state):
-#     st.title(":wrench: Settings")
-#     display_state_values(state)
-#
-#     st.write("---")
-#     options = ["Hello", "World", "Goodbye"]
-#     state.input = st.text_input("Set input value.", state.input or "")
-#     state.slider = st.slider("Set slider value.", 1, 10, state.slider)
-#     state.radio = st.radio("Set radio value.", options, options.index(state.radio) if state.radio else 0)
-#     state.checkbox = st.checkbox("Set checkbox value.", state.checkbox)
-#     state.selectbox = st.selectbox("Select value.", options, options.index(state.selectbox) if state.selectbox else 0)
-#     state.multiselect = st.multiselect("Select value(s).", options, state.multiselect)
-#
-#     # Dynamic state assignments
-#     for i in range(3):
-#         key = f"State value {i}"
-#         state[key] = st.slider(f"Set value {i}", 1, 10, state[key])
-#
-#
-# def display_state_values(state):
-#     st.write("Input state:", state.input)
-#     st.write("Slider state:", state.slider)
-#     st.write("Radio state:", state.radio)
-#     st.write("Checkbox state:", state.checkbox)
-#     st.write("Selectbox state:", state.selectbox)
-#     st.write("Multiselect state:", state.multiselect)
-#
-#     for i in range(3):
-#         st.write(f"Value {i}:", state[f"State value {i}"])
-#
-#     if st.button("Clear state"):
-#         state.clear()
-#
-#
-# class _SessionState:
-#
-#     def __init__(self, session, hash_funcs):
-#         """Initialize SessionState instance."""
-#         self.__dict__["_state"] = {
-#             "data": {},
-#             "hash": None,
-#             "hasher": _CodeHasher(hash_funcs),
-#             "is_rerun": False,
-#             "session": session,
-#         }
-#
-#     def __call__(self, **kwargs):
-#         """Initialize state data once."""
-#         for item, value in kwargs.items():
-#             if item not in self._state["data"]:
-#                 self._state["data"][item] = value
-#
-#     def __getitem__(self, item):
-#         """Return a saved state value, None if item is undefined."""
-#         return self._state["data"].get(item, None)
-#
-#     def __getattr__(self, item):
-#         """Return a saved state value, None if item is undefined."""
-#         return self._state["data"].get(item, None)
-#
-#     def __setitem__(self, item, value):
-#         """Set state value."""
-#         self._state["data"][item] = value
-#
-#     def __setattr__(self, item, value):
-#         """Set state value."""
-#         self._state["data"][item] = value
-#
-#     def clear(self):
-#         """Clear session state and request a rerun."""
-#         self._state["data"].clear()
-#         self._state["session"].request_rerun()
-#
-#     def sync(self):
-#         """Rerun the app with all state values up to date from the beginning to fix rollbacks."""
-#
-#         # Ensure to rerun only once to avoid infinite loops
-#         # caused by a constantly changing state value at each run.
-#         #
-#         # Example: state.value += 1
-#         if self._state["is_rerun"]:
-#             self._state["is_rerun"] = False
-#
-#         elif self._state["hash"] is not None:
-#             if self._state["hash"] != self._state["hasher"].to_bytes(self._state["data"], None):
-#                 self._state["is_rerun"] = True
-#                 self._state["session"].request_rerun()
-#
-#         self._state["hash"] = self._state["hasher"].to_bytes(self._state["data"], None)
-#
-#
-# def _get_session():
-#     session_id = get_report_ctx().session_id
-#     session_info = Server.get_current()._get_session_info(session_id)
-#
-#     if session_info is None:
-#         raise RuntimeError("Couldn't get your Streamlit Session object.")
-#
-#     return session_info.session
-#
-#
-# def _get_state(hash_funcs=None):
-#     session = _get_session()
-#
-#     if not hasattr(session, "_custom_session_state"):
-#         session._custom_session_state = _SessionState(session, hash_funcs)
-#
-#     return session._custom_session_state
-#
-#
-# if __name__ == "__main__":
-#     main()
 
